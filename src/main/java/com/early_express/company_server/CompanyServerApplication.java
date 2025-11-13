0ef2607e38d59b9404141f354e4e1eeef051b690
jun <pickjog@gmail.com>
2025-11-13 18:59:23 +0900

    first commit

---
+++ src/main/java/com/early_express/company_server/CompanyServerApplication.java
@@ -0,0 +1,13 @@
+package com.early_express.company_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/early_express/company_server/CompanyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.early_express.company_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
